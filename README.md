9f446f88caaba38e6fae79de039f2d2406d39836
error401de <github@anroba.de>
2021-02-13 17:51:26 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # speedtest-monitoring
-Collects and saves speedtest-cli results
+Collects results from speedtest-cli and saves them to csv file for historical analysis
+
+## setup
+Download speedtest monitoring
+
+Download https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py and place it in the same directory
+
+Set any speedtest-cli parameters (e.g. --no-download) in `speedtestParameters` variable. If down- or upload check is disabled, CSV export will contain 2 instead of 3 columns
+
+## usage
+Create cron job with a frequency of your choice
+
+Results will be saved to `output.csv`
---
+++ speedtest-monitoring.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import os, re, datetime
+
+speedtestCliFile = 'speedtest.py'
+speedtestParameters = '' 
+
+speedtest = os.popen("python " + speedtestCliFile + " " + speedtestParameters).read()
+
+downloadOutput = False
+uploadOutput = False
+writeOutput = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+download = re.search('Download: (.*?) ', speedtest)
+if download:
+	downloadOutput = ";" + download.group(1)
+
+upload = re.search('Upload: (.*?) ', speedtest)
+if upload:
+	uploadOutput = ";" + upload.group(1)
+
+if downloadOutput:
+	writeOutput = writeOutput + downloadOutput
+
+if uploadOutput:
+	writeOutput = writeOutput + uploadOutput
+
+with open('output.csv', 'a+') as file:
+	file.write(writeOutput + '\n')
